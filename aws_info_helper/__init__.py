b4a784f8b76add503dda99bbc0132fb49a08a2e8
Ken <kenjyco@gmail.com>
2018-11-14 09:25:28 -0600

    Update get_all_instances_* methods to have option to cache instances
    Also add get_cached_instances method

--- aws_info_helper/__init__.py
+++ aws_info_helper/__init__.py
@@ -14,17 +14,32 @@ class EC2(object):
         session = boto3.Session(profile_name=profile_name)
         self._ec2_client = session.client('ec2')
         self._ec2_resource = session.resource('ec2')
+        self._instances = []
 
-    def get_all_instances_full_data(self):
-        """Get all instances with full data"""
+    def get_all_instances_full_data(self, cache=False):
+        """Get all instances with full data
+
+        - cache: if True, cache results in self._instances
+        """
         instances = []
         for x in self._ec2_client.describe_instances()['Reservations']:
             instances.extend(x['Instances'])
+        if cache:
+            self._instances = instances
         return instances
 
-    def get_all_instances_filtered_data(self):
-        """Get all instances with filtered on EC2_INSTANCE_KEYS"""
-        return [
+    def get_all_instances_filtered_data(self, cache=False):
+        """Get all instances filtered on EC2_INSTANCE_KEYS
+
+        - cache: if True, cache results in self._instances
+        """
+        instances = [
             ih.filter_keys(instance, *EC2_INSTANCE_KEYS)
             for instance in self.get_all_instances_full_data()
         ]
+        if cache:
+            self._instances = instances
+        return instances
+
+    def get_cached_instances(self):
+        return self._instances
