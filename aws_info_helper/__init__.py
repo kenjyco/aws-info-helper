5535b2c595252da64b088edba12cf5783da2f52d
Ken <kenjyco@gmail.com>
2018-11-14 09:28:56 -0600

    Add show_instance_info method and add EC2_INSTANCE_INFO_FORMAT setting

--- aws_info_helper/__init__.py
+++ aws_info_helper/__init__.py
@@ -7,6 +7,7 @@ from pprint import pprint
 
 get_setting = sh.settings_getter(__name__)
 EC2_INSTANCE_KEYS = get_setting('EC2_INSTANCE_KEYS')
+EC2_INSTANCE_INFO_FORMAT = get_setting('EC2_INSTANCE_INFO_FORMAT')
 
 
 class EC2(object):
@@ -43,3 +44,20 @@ class EC2(object):
 
     def get_cached_instances(self):
         return self._instances
+
+    def show_instance_info(self, item_format=EC2_INSTANCE_INFO_FORMAT):
+        """Show info about cached instances
+
+        - item_format: Format string for lines of output (default from
+          EC2_INSTANCE_INFO_FORMAT setting)
+
+        If no cached info is found, fetch it using
+        self.get_all_instances_filtered_data
+        """
+        if not self._instances:
+            self.get_all_instances_filtered_data(cache=True)
+        make_string = ih.get_string_maker(item_format)
+        print('\n'.join([
+            make_string(instance)
+            for instance in self._instances
+        ]))
