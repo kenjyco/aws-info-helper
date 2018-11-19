a1ad7cd9f122e090038373771cf52409089004ac
Ken <kenjyco@gmail.com>
2018-11-19 01:28:25 -0600

    Update ah-xx-ec2 commands to accept --non-interactive and --profile options

--- aws_info_helper/scripts/ec2_info.py
+++ aws_info_helper/scripts/ec2_info.py
@@ -4,12 +4,20 @@ from aws_info_helper import EC2
 
 
 @click.command()
-def main():
+@click.option(
+    '--non-interactive', '-n', 'non_interactive', is_flag=True, default=False,
+    help='Do not start an ipython session at the end'
+)
+@click.option(
+    '--profile', '-p' 'profile', default='default',
+    help='Name of AWS profile to use'
+)
+def main(**kwargs):
     """Get info about EC2 instances"""
-    ec2 = EC2()
-    print('Fetching info about EC2 instances...')
+    ec2 = EC2(kwargs['profile'])
     ec2.show_instance_info(cache=True)
-    ih.start_ipython(ec2=ec2)
+    if kwargs['non_interactive'] is not True:
+        ih.start_ipython(ec2=ec2)
 
 
 if __name__ == '__main__':
--- aws_info_helper/scripts/ec2_update_collection.py
+++ aws_info_helper/scripts/ec2_update_collection.py
@@ -4,12 +4,20 @@ from aws_info_helper import EC2, AWS_EC2
 
 
 @click.command()
-def main():
+@click.option(
+    '--non-interactive', '-n', 'non_interactive', is_flag=True, default=False,
+    help='Do not start an ipython session at the end'
+)
+@click.option(
+    '--profile', '-p' 'profile', default='default',
+    help='Name of AWS profile to use'
+)
+def main(**kwargs):
     """Update info in AWS_EC2 redis-helper collection"""
-    ec2 = EC2()
-    print('Updating info in AWS_EC2 redis-helper collection...')
+    ec2 = EC2(kwargs['profile'])
     ec2.update_collection()
-    ih.start_ipython(ec2=AWS_EC2)
+    if kwargs['non_interactive'] is not True:
+        ih.start_ipython(ec2=AWS_EC2)
 
 
 if __name__ == '__main__':
